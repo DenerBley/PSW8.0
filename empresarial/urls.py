1ab259adf7ff8a6e37b0a331ee558f191dfe3e48
DenerBley <deneriot@outlook.com>
2023-10-09 11:45:32 -0300

    finalizado

---
+++ empresarial/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('gerenciar_clientes/', views.gerenciar_clientes, name="gerenciar_clientes"),
+    path('cliente/<int:cliente_id>', views.cliente, name="cliente"),
+    path('exame_cliente/<int:exame_id>', views.exame_cliente, name="exame_cliente"),
+    
+    
+
+]
